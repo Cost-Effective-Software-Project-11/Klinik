acf91b7099ec60038ba69f482043c26c3f20e13c
Krasimir Iliev <kvi_2012@abv.bg>
2024-05-23 19:13:43 +0300

    implement trials button for patians (#3)
    * implement trials button for patians
    
    * update files

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gp5/screens/patient_trials/patient_trials_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  static const double _dividerThickness = 2.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home Screen',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.teal.shade600,
+        centerTitle: true,
+        elevation: 4.00,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1.0),
+        ),
+        child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildTrialsButton(context),
+              _customDivider(
+                thickness: _dividerThickness,
+              ),
+              // Add validation if the user is a doctor to see this button:
+              // Add CreateTrial Button here:
+              const Text("Create Trial"),
+              _customDivider(
+                thickness: _dividerThickness,
+              ),
+              // Add Help Button here:
+              const Text("Help"),
+              _customDivider(
+                thickness: _dividerThickness,
+              ),
+              // Add Setting Button here:
+              const Text("Setting"),
+              _customDivider(
+                thickness: _dividerThickness,
+              ),
+              // Add Info Button here:
+              const Text("Info"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+//Patient Trials button
+  Widget _buildTrialsButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const PatientTrialsScreen()),
+        );
+      },
+      child: Text(
+        'Trials',
+        style: TextStyle(
+          fontSize: 20,
+          inherit: true,
+          fontWeight: FontWeight.bold,
+          color: Colors.deepPurple.shade900,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+//Custom divider
+  Divider _customDivider({
+    double thickness = 0.0,
+    Color color = Colors.black,
+    double indent = 0.0,
+    double endIndent = 0.0,
+    double height = 0.0,
+  }) {
+    return Divider(
+      thickness: thickness,
+      color: color,
+      indent: indent,
+      endIndent: endIndent,
+      height: height,
+    );
+  }
+}
---
+++ lib/screens/patient_trials/patient_trials_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PatientTrialsScreen extends StatelessWidget {
+  const PatientTrialsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("trials");
+  }
+}
