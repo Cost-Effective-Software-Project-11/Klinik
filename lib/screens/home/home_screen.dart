cced56082eddf1646d315f8a463c098290bfafe8
Anton Todorov <todorovanton88@gmail.com>
2024-05-25 02:33:35 +0300

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_gp5/screens/doctor_trials/doctor_trials_button.dart';
 import 'package:flutter_gp5/screens/patient_trials/patient_trials_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+  
   static const double _dividerThickness = 2.0;
+  static FirebaseAuth user = FirebaseAuth.instance;
 
+  static Future<DocumentSnapshot<Map<String, dynamic>>> userDoc = FirebaseFirestore.instance
+          .collection('doctors')
+          .doc(user.currentUser?.uid)
+          .get();
+
+  Future<Map<String, dynamic>> processData() async {
+    DocumentSnapshot snapshot = await userDoc;
+    Map<String, dynamic> userData = snapshot.data() as Map<String, dynamic>;
+
+    return userData;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +47,16 @@ class HomeScreen extends StatelessWidget {
                 thickness: _dividerThickness,
               ),
               // Add validation if the user is a doctor to see this button:
-              // Add CreateTrial Button here:
-              const Text("Create Trial"),
+              FutureBuilder<Map<String, dynamic>>(
+                future: processData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData && snapshot.data!['Doctor']) {
+                    return _buildCreateTrialsButton(context);
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
               _customDivider(
                 thickness: _dividerThickness,
               ),
@@ -55,7 +79,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-//Patient Trials button
+  //Patient Trials button
   Widget _buildTrialsButton(BuildContext context) {
     return TextButton(
       onPressed: () {
@@ -71,6 +95,28 @@ class HomeScreen extends StatelessWidget {
           inherit: true,
           fontWeight: FontWeight.bold,
           color: Colors.deepPurple.shade900,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+//Doctor Trials button
+  Widget _buildCreateTrialsButton(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const DoctorTrialsScreen()),
+        );
+      },
+      child: Text(
+        'Create Trial',
+        style: TextStyle(
+          fontSize: 20,
+          inherit: true,
+          fontWeight: FontWeight.bold,
+          color: Colors.orange.shade600,
         ),
         textAlign: TextAlign.center,
       ),
