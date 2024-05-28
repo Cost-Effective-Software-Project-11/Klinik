79d9b8deb7b3a9042cefc59706ab5bd4e4367b0e
Jorjo77 <jorjo7@abv.bg>
2024-05-28 18:44:24 +0300

    refactoredExtentionOnBuildContext

--- lib/extensions/build_context_extensions.dart
+++ lib/extensions/build_context_extensions.dart
@@ -1,14 +1,40 @@
+// ignore_for_file: prefer_conditional_assignment
+
 import 'package:flutter/material.dart';
 
+class _MediaQueryCache {
+  static double? _screenWidth;
+  static double? _screenHeight;
+
+  static double getScreenWidth(BuildContext context) {
+    if (_screenWidth == null) {
+      _screenWidth = MediaQuery.of(context).size.width;
+    }
+    return _screenWidth!;
+  }
+
+  static double getScreenHeight(BuildContext context) {
+    if (_screenHeight == null) {
+      _screenHeight = MediaQuery.of(context).size.height;
+    }
+    return _screenHeight!;
+  }
+}
+
 extension BuildContextExtensions on BuildContext {
-   // Convert width percentage to pixel value
   double widthPercentage(double percentage) {
     assert(percentage >= 0 && percentage <= 100);
-    return MediaQuery.of(this).size.width * (percentage / 100);
+    double screenWidth = _MediaQueryCache.getScreenWidth(this);
+    return screenWidth * _getDecimalPercentage(percentage);
   }
-  // Convert height percentage to pixel value
+
   double heightPercentage(double percentage) {
     assert(percentage >= 0 && percentage <= 100);
-    return MediaQuery.of(this).size.height * (percentage / 100);
+    double screenHeight = _MediaQueryCache.getScreenHeight(this);
+    return screenHeight * _getDecimalPercentage(percentage);
+  }
+
+  double _getDecimalPercentage(double percentage) {
+    return percentage / 100;
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gp5/screens/patient_trials/patient_trials_screen.dart';
-import 'package:flutter_gp5/extensions/build_context_extensions.dart';  // Import the extensions
+import 'package:flutter_gp5/extensions/build_context_extensions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  static const double _dividerThickness = 2.0;
-
   @override
   Widget build(BuildContext context) {
-    // Example percentage values
-    double buttonWidth = context.widthPercentage(80); // 80% of screen width
-    double buttonHeight = context.heightPercentage(10); // 10% of screen height
+    double containerWidth = context.widthPercentage(60); // 50% of screen width
+    double containerHeight = context.heightPercentage(30); // 30% of screen height
 
     return Scaffold(
       appBar: AppBar(
@@ -23,82 +19,25 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         elevation: 4.00,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black, width: 1.0),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildTrialsButton(context, buttonWidth, buttonHeight),
-            _customDivider(
-              thickness: _dividerThickness,
+      body: Center(
+        child: Container(
+          width: containerWidth,
+          height: containerHeight,
+          color: Colors.blue,
+          child: const Center(
+            child: Text(
+              '50% width, 30% height',
+              style: TextStyle(color: Colors.white),
             ),
-            // Add validation if the user is a doctor to see this button:
-            // Add CreateTrial Button here:
-            const Text("Create Trial"),
-            _customDivider(
-              thickness: _dividerThickness,
-            ),
-            // Add Help Button here:
-            const Text("Help"),
-            _customDivider(
-              thickness: _dividerThickness,
-            ),
-            // Add Setting Button here:
-            const Text("Setting"),
-            _customDivider(
-              thickness: _dividerThickness,
-            ),
-            // Add Info Button here:
-            const Text("Info"),
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Patient Trials button
-  Widget _buildTrialsButton(BuildContext context, double width, double height) {
-    // ignore: sized_box_for_whitespace
-    return Container(
-      width: width,
-      height: height,
-      child: TextButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const PatientTrialsScreen()),
-          );
-        },
-        child: Text(
-          'Trials',
-          style: TextStyle(
-            fontSize: 20,
-            inherit: true,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepPurple.shade900,
           ),
-          textAlign: TextAlign.center,
         ),
       ),
     );
   }
+}
 
-  // Custom divider
-  Divider _customDivider({
-    double thickness = 0.0,
-    Color color = Colors.black,
-    double indent = 0.0,
-    double endIndent = 0.0,
-    double height = 0.0,
-  }) {
-    return Divider(
-      thickness: thickness,
-      color: color,
-      indent: indent,
-      endIndent: endIndent,
-      height: height,
-    );
-  }
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
 }
