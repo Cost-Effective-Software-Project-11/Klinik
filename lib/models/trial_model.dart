ce207e793672c82ed7ec83025d7d699e761871db
Krasimir Iliev <kvi_2012@abv.bg>
2024-05-29 20:03:17 +0300

    update patient_trials_screen and models, also add some sample data

--- lib/models/trial_model.dart
+++ lib/models/trial_model.dart
@@ -3,17 +3,20 @@ import 'package:flutter_gp5/models/question_model.dart';
 class TrialModel {
   String id;
   String name;
+  String description;
   List<QuestionModel> questions = [];
 
   TrialModel(
     this.id,
     this.name,
+    this.description,
     this.questions,
   );
 
   //Getters
   String get getId => id;
   String get getName => name;
+  String get getDescription => description;
   List<QuestionModel> get getAllQuestions => questions;
   int get questionsCount => questions.length;
 
@@ -24,6 +27,10 @@ class TrialModel {
 
   set setName(String name) {
     this.name = name;
+  }
+
+  set setDescription(String description) {
+    this.description = description;
   }
 
   set setSungleQuestion(QuestionModel question) {
@@ -39,6 +46,7 @@ class TrialModel {
     return {
       'id': id,
       'name': name,
+      'description': description,
       'questions': questions.map((question) => question.toJson()).toList(),
     };
   }
@@ -48,6 +56,7 @@ class TrialModel {
     return TrialModel(
       json['id'],
       json['name'],
+      json['description'],
       (json['questions'] as List<dynamic>)
           .map((questionJson) => QuestionModel.fromJson(questionJson))
           .toList(),
