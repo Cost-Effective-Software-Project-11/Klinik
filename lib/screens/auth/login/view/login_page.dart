d8f23167ccf1f08553be45c80814af6ea0f36f5f
Victor Agovski <viktorgbs4@gmail.com>
2024-06-05 18:34:09 +0300

    Refactored code:
    - empty files are deleted
    - '-' is replaced with '_'
    - refactored main.dart

--- lib/screens/auth/login/view/login_page.dart
+++ lib/screens/auth/login/view/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gp5/login/login.dart';
 
 import '../bloc/login_bloc.dart';
 import 'login_form.dart';
@@ -9,24 +8,25 @@ import 'login_form.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  static Route<void> route() {
-    return MaterialPageRoute<void>(builder: (_) => const LoginPage());
+  @override
+  Widget build(BuildContext context) {
+    final authenticationRepository = RepositoryProvider.of<AuthenticationRepository>(context);
+    return BlocProvider(
+      create: (context) => LoginBloc(authenticationRepository: authenticationRepository),
+      child: const _LoginPage(),
+    );
   }
+}
+
+class _LoginPage extends StatelessWidget {
+  const _LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: BlocProvider(
-          create: (context) {
-            return LoginBloc(
-              authenticationRepository:
-              RepositoryProvider.of<AuthenticationRepository>(context),
-            );
-          },
-          child: const LoginForm(),
-        ),
+        child: const LoginForm(),
       ),
     );
   }
