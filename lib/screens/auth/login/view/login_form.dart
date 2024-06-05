abe981c1dde0f07bbb8a6627d9df6a7c0dcc31e4
Victor Agovski <viktorgbs4@gmail.com>
2024-06-05 18:34:09 +0300

    Refactored code:
    - empty files are deleted
    - '-' is replaced with '_'
    - refactored main.dart

--- lib/screens/auth/login/view/login_form.dart
+++ lib/screens/auth/login/view/login_form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gp5/login/login.dart';
 import 'package:formz/formz.dart';
 
 import '../bloc/login_bloc.dart';
