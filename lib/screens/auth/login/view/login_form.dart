2655988549a9a27bb31555702c8e0f70e9f5d80f
Victor Agovski <viktorgbs4@gmail.com>
2024-06-07 18:49:13 +0300

    Arranged directory

--- lib/screens/auth/login/view/login_form.dart
+++ lib/screens/auth/login/view/login_form.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
 
-import '../../bloc/login_bloc.dart';
+import '../bloc/login_bloc.dart';
 
 class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
