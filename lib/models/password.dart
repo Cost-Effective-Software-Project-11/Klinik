f1efb8e2a02709be11e64e3d2d63227c44b86caa
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:43:42 +0300

    Formz deleted from pubspec

--- lib/models/password.dart
+++ lib/models/password.dart
@@ -1,12 +1,6 @@
-import 'package:formz/formz.dart';
-
 enum PasswordValidationError { empty }
 
-class Password extends FormzInput<String, PasswordValidationError> {
-  const Password.pure() : super.pure('');
-  const Password.dirty([super.value = '']) : super.dirty();
-
-  @override
+class Password {
   PasswordValidationError? validator(String value) {
     if (value.isEmpty) return PasswordValidationError.empty;
     return null;
--- lib/models/username.dart
+++ lib/models/username.dart
@@ -1,12 +1,6 @@
-import 'package:formz/formz.dart';
-
 enum UsernameValidationError { empty }
 
-class Username extends FormzInput<String, UsernameValidationError> {
-  const Username.pure() : super.pure('');
-  const Username.dirty([super.value = '']) : super.dirty();
-
-  @override
+class Username {
   UsernameValidationError? validator(String value) {
     if (value.isEmpty) return UsernameValidationError.empty;
     return null;
