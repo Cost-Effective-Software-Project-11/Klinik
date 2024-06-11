5886eb828ef35abf81ee2f647d9c41f86515c64d
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:40:14 +0300

    Changes to login_bloc and login_state

--- lib/screens/auth/login/bloc/login_bloc.dart
+++ lib/screens/auth/login/bloc/login_bloc.dart
@@ -1,12 +1,10 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:formz/formz.dart';
-
-import '../../../../models/username.dart';
-import '../../../../models/password.dart';
+import 'package:flutter_gp5/screens/auth/login/bloc/status_enum.dart';
 
 part 'login_event.dart';
+
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
@@ -14,54 +12,21 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     required AuthenticationRepository authenticationRepository,
   })  : _authenticationRepository = authenticationRepository,
         super(const LoginState()) {
-    on<LoginUsernameChanged>(_onUsernameChanged);
-    on<LoginPasswordChanged>(_onPasswordChanged);
     on<LoginSubmitted>(_onSubmitted);
   }
 
   final AuthenticationRepository _authenticationRepository;
 
-  void _onUsernameChanged(
-      LoginUsernameChanged event,
-      Emitter<LoginState> emit,
-      ) {
-    final username = Username.dirty(event.username);
-    emit(
-      state.copyWith(
-        username: username,
-        isValid: Formz.validate([state.password, username]),
-      ),
-    );
-  }
-
-  void _onPasswordChanged(
-      LoginPasswordChanged event,
-      Emitter<LoginState> emit,
-      ) {
-    final password = Password.dirty(event.password);
-    emit(
-      state.copyWith(
-        password: password,
-        isValid: Formz.validate([password, state.username]),
-      ),
-    );
-  }
-
-  Future<void> _onSubmitted(
-      LoginSubmitted event,
-      Emitter<LoginState> emit,
-      ) async {
-    if (state.isValid) {
-      emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
-      try {
-        await _authenticationRepository.logIn(
-          username: state.username.value,
-          password: state.password.value,
-        );
-        emit(state.copyWith(status: FormzSubmissionStatus.success));
-      } catch (_) {
-        emit(state.copyWith(status: FormzSubmissionStatus.failure));
-      }
+  Future<void> _onSubmitted(LoginSubmitted event, Emitter<LoginState> emit) async {
+    emit(state.copyWith(status: StatusEnum.inProgress));
+    try {
+      await _authenticationRepository.logIn(
+        username: state.username,
+        password: state.password,
+      );
+      emit(state.copyWith(status: StatusEnum.success));
+    } catch (_) {
+      emit(state.copyWith(status: StatusEnum.failure));
     }
   }
 }
