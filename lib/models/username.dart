3a6ecd79766360c53b81e801838a6255e1e0bdab
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:43:42 +0300

    Removed override from validator method

--- lib/models/username.dart
+++ lib/models/username.dart
@@ -1,7 +1,6 @@
 enum UsernameValidationError { empty }
 
 class Username {
-  @override
   UsernameValidationError? validator(String value) {
     if (value.isEmpty) return UsernameValidationError.empty;
     return null;
