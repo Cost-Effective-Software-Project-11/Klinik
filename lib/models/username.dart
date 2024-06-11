9310e4fd6354c7c7a7792dc2009071c7cd7a2f55
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:41:04 +0300

    Deleted third dependency related code in the models

--- lib/models/username.dart
+++ lib/models/username.dart
@@ -1,11 +1,6 @@
-import 'package:formz/formz.dart';
-
 enum UsernameValidationError { empty }
 
-class Username extends FormzInput<String, UsernameValidationError> {
-  const Username.pure() : super.pure('');
-  const Username.dirty([super.value = '']) : super.dirty();
-
+class Username {
   @override
   UsernameValidationError? validator(String value) {
     if (value.isEmpty) return UsernameValidationError.empty;
