cf1fbe809f9f13ec6ca288aee7d5d0863872b126
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:41:04 +0300

    Changes

--- lib/models/password.dart
+++ lib/models/password.dart
@@ -1,6 +1,7 @@
 enum PasswordValidationError { empty }
 
 class Password {
+  @override
   PasswordValidationError? validator(String value) {
     if (value.isEmpty) return PasswordValidationError.empty;
     return null;
