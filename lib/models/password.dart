6719ae8c376834f7e71a9c6309587fde89e343e4
Victor Agovski <viktorgbs4@gmail.com>
2024-06-11 21:43:42 +0300

    Removed override from validator method

--- lib/models/password.dart
+++ lib/models/password.dart
@@ -1,7 +1,6 @@
 enum PasswordValidationError { empty }
 
 class Password {
-  @override
   PasswordValidationError? validator(String value) {
     if (value.isEmpty) return PasswordValidationError.empty;
     return null;
