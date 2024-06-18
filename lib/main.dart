ee6d8162e49373203019c6acc4ea65e835387224
Victor Agovski <viktorgbs4@gmail.com>
2024-06-18 14:26:37 +0300

    "register" directory renamed to "signup"

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gp5/screens/auth/bloc/authentication_bloc.dart';
+import 'package:flutter_gp5/screens/auth/signup/signup_screen.dart';
 import 'package:user_repository/user_repository.dart';
 import 'package:flutter_gp5/screens/home/home_screen.dart';
 import 'package:flutter_gp5/screens/auth/login/login_screen.dart';
-import 'package:flutter_gp5/screens/auth/register/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'GP5',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
             useMaterial3: true,
           ),
           initialRoute: '/',
