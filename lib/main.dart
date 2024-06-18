dff04a7dfc73ffa81d9a4331ab3b34dca1250b98
Victor Agovski <viktorgbs4@gmail.com>
2024-06-18 18:40:10 +0300

    Changed color

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           title: 'GP5',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
           initialRoute: '/',
