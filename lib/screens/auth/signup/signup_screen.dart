298a6e1cfc6172d1f85cf222d0f1c00fc55148a4
Victor Agovski <viktorgbs4@gmail.com>
2024-07-03 00:00:56 +0300

    "A value for optional parameter 'key' isn't ever given.". I put const _SignupScreen({Key? key}) : super(key: key); again

--- lib/screens/auth/signup/signup_screen.dart
+++ lib/screens/auth/signup/signup_screen.dart
@@ -20,7 +20,7 @@ class SignupScreen extends StatelessWidget {
 }
 
 class _SignupScreen extends StatefulWidget {
-  const _SignupScreen({super.key});
+  const _SignupScreen({Key? key}) : super(key: key);
 
   @override
   State<_SignupScreen> createState() => _SignupScreenState();
