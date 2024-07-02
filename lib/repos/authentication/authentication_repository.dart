85f56f546f58add75063accaececf4693f8feca0
Victor Agovski <viktorgbs4@gmail.com>
2024-07-02 19:06:08 +0300

    Authentication and user repos moved to lib folder

--- lib/repos/authentication/authentication_repository.dart
+++ lib/repos/authentication/authentication_repository.dart
@@ -2,18 +2,24 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../enums/authentication_status_enum.dart';
+
+// Define the authentication status as an enumeration.
 enum _AuthenticationStatus { authenticated, unauthenticated }
 
 class AuthenticationRepository {
-  final FirebaseAuth _firebaseAuth;
-  final FirebaseFirestore _firestore;
+  final FirebaseAuth _firebaseAuth; // Instance of FirebaseAuth.
+  final FirebaseFirestore _firestore; // Instance of FirebaseFirestore.
+  // StreamController to handle the authentication status updates.
   final StreamController<_AuthenticationStatus> _controller = StreamController<_AuthenticationStatus>();
 
+  // Constructor with optional parameters for FirebaseAuth and FirebaseFirestore.
   AuthenticationRepository({
     FirebaseAuth? firebaseAuth,
     FirebaseFirestore? firestore,
   })  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
         _firestore = firestore ?? FirebaseFirestore.instance {
+    // Listen to the auth state changes and update the stream accordingly.
     _firebaseAuth.authStateChanges().listen((user) {
       if (user != null) {
         _controller.add(_AuthenticationStatus.authenticated);
@@ -23,22 +29,25 @@ class AuthenticationRepository {
     });
   }
 
-  Stream<_AuthenticationStatus> get status async* {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    yield _firebaseAuth.currentUser != null
-        ? _AuthenticationStatus.authenticated
-        : _AuthenticationStatus.unauthenticated;
-    yield* _controller.stream;
+  // Stream that provides continuous updates on the authentication status.
+  Stream<AuthenticationStatus> get status {
+    // Ensure this stream emits AuthenticationStatus
+    return _firebaseAuth.authStateChanges().map((user) {
+      return user != null ? AuthenticationStatus.authenticated : AuthenticationStatus.unauthenticated;
+    });
   }
 
+
+  // Get the current user.
   User? get currentUser => _firebaseAuth.currentUser;
 
+  // Method to log in a user using a username and password.
   Future<void> logIn({
     required String username,
     required String password,
   }) async {
     try {
-      // Fetch the document by username
+      // Query Firestore for the user by username.
       var querySnapshot = await _firestore
           .collection('users')
           .where('username', isEqualTo: username)
@@ -52,13 +61,11 @@ class AuthenticationRepository {
       var email = userDoc['email'];
       var storedPassword = userDoc['password'];
 
-      print('Email: $email, Stored Password: $storedPassword, Input Password: $password');
-
       if (storedPassword != password) {
         throw Exception('Incorrect password');
       }
 
-      // Sign in with email and password
+      // Sign in with email and password.
       await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
       _controller.add(_AuthenticationStatus.authenticated);
     } catch (e) {
@@ -68,25 +75,25 @@ class AuthenticationRepository {
     }
   }
 
+  // Method to sign up a new user.
   Future<void> signUp({
     required String username,
     required String email,
     required String password,
   }) async {
     try {
-      print("Signing up user with email: $email");
+      // Create user with email and password.
       UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
-      print("User signed up with UID: ${userCredential.user!.uid}");
+      // Store user information in Firestore.
       await _firestore.collection('users').doc(userCredential.user!.uid).set({
         'username': username,
         'email': email,
         'password': password,
       });
       _controller.add(_AuthenticationStatus.authenticated);
-      print("User data stored in Firestore");
     } catch (e) {
       print("Signup error: $e");
       _controller.add(_AuthenticationStatus.unauthenticated);
@@ -94,16 +101,18 @@ class AuthenticationRepository {
     }
   }
 
+  // Method to check if user is currently logged in.
   bool isLoggedIn() {
     var isLoggedIn = _firebaseAuth.currentUser != null;
-    print('Current user: ${_firebaseAuth.currentUser}');
     return isLoggedIn;
   }
 
+  // Method to log out the current user.
   Future<void> logOut() async {
     await _firebaseAuth.signOut();
     _controller.add(_AuthenticationStatus.unauthenticated);
   }
 
+  // Dispose method to close the StreamController when done.
   void dispose() => _controller.close();
 }
--- lib/screens/auth/bloc/authentication_bloc.dart
+++ lib/screens/auth/bloc/authentication_bloc.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
 
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:user_repository/user_repository.dart';
 
 import '../../../enums/authentication_status_enum.dart';
+import '../../../repos/authentication/authentication_repository.dart';
+import '../../../repos/user/models/user.dart';
+import '../../../repos/user/user_repository.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
+/// Bloc to handle authentication state changes and manage user authentication.
 class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus> _authenticationStatusSubscription;
+
+  /// Constructor initializing the bloc with repositories and states.
   AuthenticationBloc({
     required AuthenticationRepository authenticationRepository,
     required UserRepository userRepository,
@@ -20,21 +27,12 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> 
     on<_AuthenticationStatusChanged>(_onAuthenticationStatusChanged);
     on<AuthenticationLogoutRequested>(_onAuthenticationLogoutRequested);
 
+    // Make sure the stream emits AuthenticationStatus and remove the casting if not needed
     _authenticationStatusSubscription = _authenticationRepository.status.listen(
-          (status) => add(_AuthenticationStatusChanged(status as AuthenticationStatus)),
-    ) as StreamSubscription<AuthenticationStatus>;
+          (status) => add(_AuthenticationStatusChanged(status)),
+    );
   }
-
-  final AuthenticationRepository _authenticationRepository;
-  final UserRepository _userRepository;
-  late StreamSubscription<AuthenticationStatus> _authenticationStatusSubscription;
-
-  @override
-  Future<void> close() {
-    _authenticationStatusSubscription.cancel();
-    return super.close();
-  }
-
+  /// Handles changes in authentication status and updates the state accordingly.
   Future<void> _onAuthenticationStatusChanged(
       _AuthenticationStatusChanged event,
       Emitter<AuthenticationState> emit,
@@ -57,6 +55,7 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> 
     }
   }
 
+  /// Logs out the current user and updates the state to unauthenticated.
   void _onAuthenticationLogoutRequested(
       AuthenticationLogoutRequested event,
       Emitter<AuthenticationState> emit,
@@ -64,6 +63,7 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> 
     _authenticationRepository.logOut();
   }
 
+  /// Attempts to fetch the currently authenticated user's details.
   Future<User?> _tryGetUser() async {
     final currentUser = _authenticationRepository.currentUser;
     if (currentUser != null) {
@@ -71,9 +71,16 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> 
         final user = await _userRepository.getUser(currentUser.uid);
         return user;
       } catch (_) {
-        return null;
+        return null;  // Return null if fetching user fails.
       }
     }
-    return null;
+    return null;  // Return null if no current user.
+  }
+
+  /// Overridden close method to cancel the stream subscription on bloc close.
+  @override
+  Future<void> close() {
+    _authenticationStatusSubscription.cancel();
+    return super.close();
   }
 }
