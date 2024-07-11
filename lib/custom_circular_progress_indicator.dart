d4881a1290c0fb064a47a2f604958ffa463fde6f
Jorjo77 <jorjo7@abv.bg>
2024-07-11 19:36:50 +0300

    approved design

--- lib/custom_circular_progress_indicator.dart
+++ lib/custom_circular_progress_indicator.dart
@@ -1,55 +1,5 @@
 import 'package:flutter/material.dart';
 
-class RoundedCircularProgressPainter extends CustomPainter {
-  final double progress; // Progress value between 0.0 and 1.0
-  final Color color; // Primary color of the progress indicator
-  final double strokeWidth; // Thickness of the progress indicator
-
-  RoundedCircularProgressPainter({
-    required this.progress,
-    required this.color,
-    this.strokeWidth = 6.0,
-  });
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint backgroundPaint = Paint()
-      ..color = color.withOpacity(0.1) // Lighten the background color
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    Paint progressPaint = Paint()
-      ..shader = LinearGradient(
-        colors: [color, color.withOpacity(0.2)], // Gradient color effect
-      ).createShader(Rect.fromCircle(center: size.center(Offset.zero), radius: size.width / 2))
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    Offset center = size.center(Offset.zero);
-    double radius = size.width / 2;
-
-    // Draw the background circle
-    canvas.drawCircle(center, radius, backgroundPaint);
-
-    // Draw the progress arc
-    double angle = 2 * 3.141592653589793238 * progress;
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius),
-      -3.141592653589793238 / 2, // Start angle
-      angle, // Sweep angle
-      false,
-      progressPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
-}
-
 class AnimatedCustomCircularProgressIndicator extends StatefulWidget {
   final double size;
   final Color color;
@@ -59,11 +9,10 @@ class AnimatedCustomCircularProgressIndicator extends StatefulWidget {
     super.key,
     this.size = 48.0,
     this.color = const Color(0xFF6750A4),
-    this.strokeWidth = 6.0,
+    this.strokeWidth = 8.0,
   });
 
   @override
-  // ignore: library_private_types_in_public_api
   _AnimatedCustomCircularProgressIndicatorState createState() => _AnimatedCustomCircularProgressIndicatorState();
 }
 
@@ -95,8 +44,8 @@ class _AnimatedCustomCircularProgressIndicatorState extends State<AnimatedCustom
         animation: _controller,
         builder: (context, child) {
           return CustomPaint(
-            painter: RoundedCircularProgressPainter(
-              progress: _controller.value,
+            painter: _CircularProgressPainter(
+              startAngle: _controller.value * 2 * 3.141592653589793238,
               color: widget.color,
               strokeWidth: widget.strokeWidth,
             ),
@@ -104,5 +53,55 @@ class _AnimatedCustomCircularProgressIndicatorState extends State<AnimatedCustom
         },
       ),
     );
+  }
+}
+
+class _CircularProgressPainter extends CustomPainter {
+  final double startAngle;
+  final Color color;
+  final double strokeWidth;
+
+  _CircularProgressPainter({
+    required this.startAngle,
+    required this.color,
+    required this.strokeWidth,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint backgroundPaint = Paint()
+      ..color = color.withOpacity(0.03)
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Paint progressPaint = Paint()
+      ..shader = LinearGradient(
+        colors: [color, color.withOpacity(0.1)],
+      ).createShader(Rect.fromCircle(center: size.center(Offset.zero), radius: size.width / 2))
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Offset center = size.center(Offset.zero);
+    double radius = size.width / 2;
+
+    // Draw the background circle
+    canvas.drawCircle(center, radius, backgroundPaint);
+
+    // Draw the progress arc
+    double sweepAngle = 3.141592653589793238 * 1.5; // Constant sweep angle for 3/4 of the circle
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      startAngle,
+      sweepAngle,
+      false,
+      progressPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
