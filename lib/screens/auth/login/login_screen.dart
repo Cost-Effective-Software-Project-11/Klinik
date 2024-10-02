56ba2447bbfc93c286870bd6d6342d68a745556a
Jorjo77 <jorjo7@abv.bg>
2024-10-02 18:27:54 +0300

    removed Remember me button from login screan

--- lib/screens/auth/login/login_screen.dart
+++ lib/screens/auth/login/login_screen.dart
@@ -39,7 +39,6 @@ class _LoginScreenState extends State<_LoginScreen> {
   final FocusNode _emailFocusNode = FocusNode();
   final FocusNode _passwordFocusNode = FocusNode();
   bool _passwordVisible = false;
-  bool _rememberMe = false;
   bool _isFormFilled = false;
 
   @override
@@ -151,7 +150,6 @@ class _LoginScreenState extends State<_LoginScreen> {
                         children: [
                           _buildSignUpForm(context),
                           _forgotPasswordButton(context),
-                          _rememberMeField(),
                           _buildLoginButton(),
                           _signupRow(context),
                         ],
@@ -415,49 +413,6 @@ class _LoginScreenState extends State<_LoginScreen> {
     );
   }
 
-  Widget _rememberMeField() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Transform.scale(
-            scale: 1.2,
-            child: Checkbox(
-              value: _rememberMe,
-              activeColor: const Color(0xFF6750A4),
-              onChanged: (bool? value) {
-                setState(() {
-                  _rememberMe = value!;
-                });
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                _rememberMe = !_rememberMe;
-              });
-            },
-            child: Text(
-              AppLocale.of(context)!.remember_me,
-              style: const TextStyle(
-                color: Color(0xFF1D1B20),
-                fontSize: 16,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w400,
-                height: 1.4,
-                letterSpacing: 0.25,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
   showRegisterAsDialog(BuildContext context) {
     showDialog(
